76c40ebb236eaaae95564df5f4d492e8f0c3c2a4
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-06-10 19:37:46 +0200

    error handler middleware

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,17 +1,15 @@
 import User from "../models/userModel.js";
+import expressAsyncHandler from "express-async-handler";
 
-export const createUser = async (req, res) => {
+export const createUser = expressAsyncHandler(async (req, res) => {
   const email = req.body.email;
 
-  const findUser = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-  if (!findUser) {
+  if (!user) {
     const newUser = await User.create(req.body);
     res.json(newUser);
   } else {
-    res.json({
-      msg: "User already exists",
-      success: false,
-    });
+    throw new Error("User already exists!");
   }
-};
+});
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { dbConnect } from "./db/connectdb.js";
 import authRouter from "./routes/authRouter.js";
+import { errorHandler, notFound } from "./middlewares/errorHandler.js";
 
 const app = express();
 dotenv.config();
@@ -12,6 +13,10 @@ const PORT = process.env.PORT || 4000;
 dbConnect();
 
 app.use("/api/user", authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`server is running at port: ${PORT}`);
 });
