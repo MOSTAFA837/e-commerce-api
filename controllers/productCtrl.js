b1a0cc8d6b8fc807a8725780424464094e2133d5
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-06-11 23:50:11 +0200

    get, delete, update products

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -3,15 +3,66 @@ import Product from "../models/productModel.js";
 
 export const createProduct = expressAsyncHandler(async (req, res) => {
   try {
-    const isProductExists = Product.findOne(req.body.slug);
-    if (isProductExists) {
-      throw new Error("product already exists");
-    }
-
     const product = await Product.create(req.body);
     res.json({
       msg: "New product has been created successfully",
       product,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const getProduct = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+    res.json(product);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const getAllProducts = expressAsyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const deleteProduct = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+
+    res.json({
+      msg: `Successfully deleted a product`,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const updateProduct = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndUpdate(id, {
+      title: req?.body?.title,
+      slug: req?.body?.slug,
+      description: req?.body?.description,
+      price: req?.body?.price,
+      category: req?.body?.category,
+      brand: req?.body?.brand,
+      quantity: req?.body?.quantity,
+    });
+
+    res.json({
+      msg: `Successfully updated a product`,
     });
   } catch (error) {
     throw new Error(error);
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { createProduct } from "../controllers/productCtrl.js";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProduct,
+  updateProduct,
+} from "../controllers/productCtrl.js";
 const router = express.Router();
 
 router.post("/", createProduct);
+router.get("/:id", getProduct);
+router.get("/", getAllProducts);
+router.delete("/:id", deleteProduct);
+router.put("/:id", updateProduct);
 
 export default router;
